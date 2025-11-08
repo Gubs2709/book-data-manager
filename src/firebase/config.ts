264a3c722bddb817d8484d1c51e9cdf903fd5621
Darshan Premraj <darshanpremraj88@gmail.com>
2025-11-08 07:33:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1563294417-a469a",
+  "appId": "1:568292832319:web:43e2e4b9cb896ba7b4b48a",
+  "apiKey": "AIzaSyCZHwiaRJMLfK967w2ct2KcTNIQSvvcQQE",
+  "authDomain": "studio-1563294417-a469a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "568292832319"
+};
